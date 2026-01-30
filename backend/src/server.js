3d8125f692d9e2680a6820b4c3f089699d17cad2
shivraj-yadav <shivrajyadav320@gmail.com>
2026-01-30 13:43:00 +0530

    db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,24 @@
-import express from 'express';
-import { ENV } from './lib/env.js';
+import express from "express";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+
 const app = express();
 const PORT = ENV.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error.message);
+  }
+
+};
+
+startServer();
