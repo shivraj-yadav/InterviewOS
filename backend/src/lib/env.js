55010d8ac3d77d3203812a29a3dbf4104018229c
shivraj-yadav <shivrajyadav320@gmail.com>
2026-01-30 14:57:39 +0530

    serverjs changes

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,5 +4,11 @@ dotenv.config({quiet: true}); // 🔥 must run immediately
 
 export const ENV = {
   PORT: process.env.PORT || 3000,
-  MONGODB_URI: process.env.MONGODB_URI
+  MONGODB_URI: process.env.MONGODB_URI,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  CLIENT_URL: process.env.CLIENT_URL
+  
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,24 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 const PORT = ENV.PORT;
 
+//Middleware 
+app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL,credentials:true}));
+app.use("/api/inngest",serve({client:inngest,functions}));
+//Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/inngest/health", (req, res) => {
+  res.json({ status: "ok", inngest: "active" });
 });
 
 const startServer = async () => {
