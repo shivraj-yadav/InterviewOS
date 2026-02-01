725bf0a4f0ddad85a3e3976f5ff0e8fd5daa1246
shivraj-yadav <shivrajyadav320@gmail.com>
2026-02-01 12:41:34 +0530

    API made robust

---
+++ backend/src/routes/chatRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getStreamToken } from "../controllers/chatControllers.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+const router = express.Router();
+
+router.get("/token", protectRoute,getStreamToken);
+export default router;
