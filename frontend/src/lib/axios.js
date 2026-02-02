84bb300c3e588758a33edee8d07c52036465b47a
shivraj-yadav <shivrajyadav320@gmail.com>
2026-02-02 16:42:36 +0530

    fix: make axios baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.PROD 
-  ? "https://interviewos-n8i0.onrender.com/api" 
+  ? import.meta.env.VITE_API_URL || "https://interviewos-n8i0.onrender.com/api" 
   : "/api";
 console.log("Axios baseURL:", baseURL);
 
 const axiosInstance = axios.create({
   baseURL,
-  withCredentials: true,
+  withCredentials: false,
 });
 
 export default axiosInstance;
